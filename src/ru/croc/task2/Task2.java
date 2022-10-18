0b37eb03f150f1ea9f7a1a3da4570b9574786639
Matiash-d-s <76624580+Matiash-d-s@users.noreply.github.com>
2022-10-18 13:59:37 +0300

    Add files via upload

--- src/ru/croc/task2/Task2.java
+++ src/ru/croc/task2/Task2.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class Task2 {
+    public static void main(String[] args) {
+        long v;
+        Scanner in = new Scanner(System.in);
+        v = in.nextLong();
+        System.out.println(printBytes(v));
+
+    }
+
+    public static String printBytes(long inBytes) {
+        int unitNumber = 0;
+        float res = inBytes;
+        String[] units = new String[] {"B","KB","MB","GB","TB","PB","EB"};
+        while (res >= 1024) {
+            res /= 1024;
+            unitNumber++;
+        }
+        return inBytes + " B -> " + String.format("%.1f",res) + " " + units[unitNumber];
+    }
+}
