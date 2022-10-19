90a79537b1503a7451b6c6c0cd15baa7be047838
Matiash-d-s <76624580+Matiash-d-s@users.noreply.github.com>
2022-10-19 16:07:16 +0300

    Update Task1.java

--- src/ru/croc/task1/Task1.java
+++ src/ru/croc/task1/Task1.java
@@ -11,32 +11,32 @@ public class Task1 {
         Point a = new Point();
         System.out.print("Введите координату х вершины №1:");
         a.x = in.nextDouble();
-        System.out.print("Ââåäèòå êîîðäèíàòó y âåðøèíû ¹1:");
+        System.out.print("Введите координату y вершины №1:");
         a.y = in.nextDouble();
 
         Point b = new Point();
-        System.out.print("Ââåäèòå êîîðäèíàòó õ âåðøèíû ¹2:");
+        System.out.print("Введите координату x вершины №2:");
         b.x = in.nextDouble();
-        System.out.print("Ââåäèòå êîîðäèíàòó y âåðøèíû ¹2:");
+        System.out.print("Введите координату y вершины №2:");
         b.y = in.nextDouble();
 
         Point c = new Point();
-        System.out.print("Ââåäèòå êîîðäèíàòó õ âåðøèíû ¹3:");
+        System.out.print("Введите координату x вершины №3:");
         c.x = in.nextDouble();
-        System.out.print("Ââåäèòå êîîðäèíàòó y âåðøèíû ¹3:");
+        System.out.print("Введите координату y вершины №3:");
         c.y = in.nextDouble();
         double ab = Math.sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));
         double ac = Math.sqrt((a.x - c.x)*(a.x - c.x) + (a.y - c.y)*(a.y - c.y));
         double bc = Math.sqrt((b.x - c.x)*(b.x - c.x) + (b.y - c.y)*(b.y - c.y));
         if (ab + ac <= bc || ab + bc <= ac || ac + bc <= ab)
-            System.out.println("Òðåóãîëüíèê íå ñóùåñòâóåò");
+            System.out.println("Треугольник не существует");
         else
         {
             double p = (ab + ac + bc) / 2.0;
             double square = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
-            System.out.println("Ïëîùàäü ïåðâûì ñïîñîáîì: " + String.format("%.1f",square));
+            System.out.println("Площадь первым способом: " + String.format("%.1f",square));
             double square2 = 0.5 * Math.abs((b.x-a.x)*(c.y-a.y)-(c.x-a.x)*(b.y-a.y));
-            System.out.println("Ïëîùàäü âòîðûì ñïîñîáîì: " + square2);
+            System.out.println("Площадь вторым способом: " + square2);
         }
         
     }
